51832ab804956fd26a97e0fcabf8e0b0c4d97241
Mohammed Nihal K <64337491+nihal350@users.noreply.github.com>
2025-04-11 20:09:33 +0200

    Add files via upload

---
+++ public-/script/carousel.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+    $('.item-image').slick({
+        arrows: true,
+        draggable: false,
+        slidesToScroll: 1,
+        slidesToShow: 2,
+        autoplay: true,
+        autoplaySpeed: 1000,
+        // prevArrow: $('#up'),
+        // nextArrow: $('#down')
+    })
+})
